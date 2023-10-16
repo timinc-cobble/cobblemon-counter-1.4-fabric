525fc10e68560204546357aa0c10f7b2e15adb15
tmetcalfe89 <tmetcalfe89@gmail.com>
2023-10-15 21:32:59 -0700

    1.4 working!

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,8 +3,6 @@ rootProject.name = "cobblemon-counter"
 pluginManagement {
     repositories {
         maven("https://maven.fabricmc.net/")
-        maven("https://maven.architectury.dev/")
-        maven("https://maven.minecraftforge.net/")
         gradlePluginPortal()
     }
 }
--- src/main/kotlin/us/timinc/mc/cobblemon/counter/Counter.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/counter/Counter.kt
@@ -6,13 +6,20 @@ import com.cobblemon.mod.common.api.events.battles.BattleFaintedEvent
 import com.cobblemon.mod.common.api.events.pokemon.PokemonCapturedEvent
 import com.cobblemon.mod.common.api.storage.player.PlayerDataExtensionRegistry
 import com.cobblemon.mod.common.util.getPlayer
+import com.mojang.brigadier.Command
+import com.mojang.brigadier.arguments.StringArgumentType
+import com.mojang.brigadier.context.CommandContext
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.entity.player.PlayerEntity
 import us.timinc.mc.cobblemon.counter.store.CaptureCount
 import us.timinc.mc.cobblemon.counter.store.CaptureStreak
 import us.timinc.mc.cobblemon.counter.store.KoCount
 import us.timinc.mc.cobblemon.counter.store.KoStreak
 import java.util.*
+import net.minecraft.server.command.CommandManager.*
+import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.text.Text
 
 object Counter : ModInitializer {
     @Suppress("unused")
@@ -24,77 +31,76 @@ object Counter : ModInitializer {
         PlayerDataExtensionRegistry.register(CaptureCount.NAME, CaptureCount::class.java)
         PlayerDataExtensionRegistry.register(CaptureStreak.NAME, CaptureStreak::class.java)
 
-//        CobblemonEvents.POKEMON_CAPTURED.subscribe { handlePokemonCapture(it) }
-//        CobblemonEvents.BATTLE_VICTORY.subscribe { handleWildDefeat(it) }
+        CobblemonEvents.POKEMON_CAPTURED.subscribe { handlePokemonCapture(it) }
         CobblemonEvents.BATTLE_FAINTED.subscribe { handleWildDefeat(it) }
-//        CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
-//            dispatcher.register(
-//                literal<CommandSourceStack>("counter")
-//                    .then(
-//                        literal<CommandSourceStack>("ko")
-//                            .then(
-//                                literal<CommandSourceStack>("count")
-//                                    .then(
-//                                        argument<CommandSourceStack, String>("species", StringArgumentType.greedyString())
-//                                            .executes { checkKoCount(it) }
-//                                    )
-//                            )
-//                            .then(
-//                                literal<CommandSourceStack>("streak")
-//                                    .executes { checkKoStreak(it) }
-//                            )
-//                    )
-//                    .then(
-//                        literal<CommandSourceStack>("capture")
-//                            .then(
-//                                literal<CommandSourceStack>("count")
-//                                    .then(
-//                                        argument<CommandSourceStack, String>("species", StringArgumentType.greedyString())
-//                                            .executes { checkCaptureCount(it) }
-//                                    )
-//                            )
-//                            .then(
-//                                literal<CommandSourceStack>("streak")
-//                                    .executes { checkCaptureStreak(it) }
-//                            )
-//                    )
-//            )
-//        }
+        CommandRegistrationCallback.EVENT.register { dispatcher, _, _ ->
+            dispatcher.register(
+                literal("counter")
+                    .then(
+                        literal("ko")
+                            .then(
+                                literal("count")
+                                    .then(
+                                        argument("species", StringArgumentType.greedyString())
+                                            .executes { checkKoCount(it) }
+                                    )
+                            )
+                            .then(
+                                literal("streak")
+                                    .executes { checkKoStreak(it) }
+                            )
+                    )
+                    .then(
+                        literal("capture")
+                            .then(
+                                literal("count")
+                                    .then(
+                                        argument("species", StringArgumentType.greedyString())
+                                            .executes { checkCaptureCount(it) }
+                                    )
+                            )
+                            .then(
+                                literal("streak")
+                                    .executes { checkCaptureStreak(it) }
+                            )
+                    )
+            )
+        }
     }
 
-//    private fun checkKoCount(ctx: CommandContext<CommandSourceStack>): Int {
-//        val player = ctx.source.player ?: return 0
-//        val species = StringArgumentType.getString(ctx, "species")
-//        val score = getPlayerKoCount(player, species)
-//        ctx.source.sendSystemMessage(Component.translatable("counter.ko.count", score, species))
-//        return Command.SINGLE_SUCCESS
-//    }
-//
-//    private fun checkKoStreak(ctx: CommandContext<CommandSourceStack>): Int {
-//        val player = ctx.source.player ?: return 0
-//        val streakData = getPlayerKoStreak(player)
-//        val species = streakData.first
-//        val score = streakData.second
-//        ctx.source.sendSystemMessage(Component.translatable("counter.ko.streak", score, species))
-//        return Command.SINGLE_SUCCESS
-//    }
-//
-//    private fun checkCaptureCount(ctx: CommandContext<CommandSourceStack>): Int {
-//        val player = ctx.source.player ?: return 0
-//        val species = StringArgumentType.getString(ctx, "species")
-//        val score = getPlayerCaptureCount(player, species)
-//        ctx.source.sendSystemMessage(Component.translatable("counter.capture.count", score, species))
-//        return Command.SINGLE_SUCCESS
-//    }
-//
-//    private fun checkCaptureStreak(ctx: CommandContext<CommandSourceStack>): Int {
-//        val player = ctx.source.player ?: return 0
-//        val streakData = getPlayerCaptureStreak(player)
-//        val species = streakData.first
-//        val score = streakData.second
-//        ctx.source.sendSystemMessage(Component.translatable("counter.capture.streak", score, species))
-//        return Command.SINGLE_SUCCESS
-//    }
+    private fun checkKoCount(ctx: CommandContext<ServerCommandSource>): Int {
+        val player = ctx.source.player ?: return 0
+        val species = StringArgumentType.getString(ctx, "species")
+        val score = getPlayerKoCount(player, species)
+        ctx.source.sendMessage(Text.translatable("counter.ko.count", score, species))
+        return Command.SINGLE_SUCCESS
+    }
+
+    private fun checkKoStreak(ctx: CommandContext<ServerCommandSource>): Int {
+        val player = ctx.source.player ?: return 0
+        val streakData = getPlayerKoStreak(player)
+        val species = streakData.first
+        val score = streakData.second
+        ctx.source.sendMessage(Text.translatable("counter.ko.streak", score, species))
+        return Command.SINGLE_SUCCESS
+    }
+
+    private fun checkCaptureCount(ctx: CommandContext<ServerCommandSource>): Int {
+        val player = ctx.source.player ?: return 0
+        val species = StringArgumentType.getString(ctx, "species")
+        val score = getPlayerCaptureCount(player, species)
+        ctx.source.sendMessage(Text.translatable("counter.capture.count", score, species))
+        return Command.SINGLE_SUCCESS
+    }
+
+    private fun checkCaptureStreak(ctx: CommandContext<ServerCommandSource>): Int {
+        val player = ctx.source.player ?: return 0
+        val streakData = getPlayerCaptureStreak(player)
+        val species = streakData.first
+        val score = streakData.second
+        ctx.source.sendMessage(Text.translatable("counter.capture.streak", score, species))
+        return Command.SINGLE_SUCCESS
+    }
 
     private fun handlePokemonCapture(event: PokemonCapturedEvent) {
         val species = event.pokemon.species.name.lowercase()
@@ -137,14 +143,14 @@ object Counter : ModInitializer {
         return (playerData.extraData.getOrPut(KoStreak.NAME) { KoStreak() } as KoStreak).get(species)
     }
 
-    @Suppress("unused")
+    @Suppress("unused", "MemberVisibilityCanBePrivate")
     fun getPlayerKoStreak(player: PlayerEntity): Pair<String, Int> {
         val playerData = Cobblemon.playerData.get(player)
         val koStreakData = (playerData.extraData.getOrPut(KoStreak.NAME) { KoStreak() } as KoStreak)
         return Pair(koStreakData.species, koStreakData.count)
     }
 
-    @Suppress("unused")
+    @Suppress("unused", "MemberVisibilityCanBePrivate")
     fun getPlayerKoCount(player: PlayerEntity, species: String): Int {
         val playerData = Cobblemon.playerData.get(player)
         return (playerData.extraData.getOrPut(KoCount.NAME) { KoCount() } as KoCount).get(species)
@@ -156,19 +162,16 @@ object Counter : ModInitializer {
         return (playerData.extraData.getOrPut(CaptureStreak.NAME) { CaptureStreak() } as CaptureStreak).get(species)
     }
 
+    @Suppress("MemberVisibilityCanBePrivate")
     fun getPlayerCaptureStreak(player: PlayerEntity): Pair<String, Int> {
         val playerData = Cobblemon.playerData.get(player)
         val captureStreakData = (playerData.extraData.getOrPut(CaptureStreak.NAME) { CaptureStreak() } as CaptureStreak)
         return Pair(captureStreakData.species, captureStreakData.count)
     }
 
-    @Suppress("unused")
+    @Suppress("unused", "MemberVisibilityCanBePrivate")
     fun getPlayerCaptureCount(player: PlayerEntity, species: String): Int {
         val playerData = Cobblemon.playerData.get(player)
         return (playerData.extraData.getOrPut(CaptureCount.NAME) { CaptureCount() } as CaptureCount).get(species)
     }
 }
-
-//fun LiteralArgumentBuilder<CommandSourceStack>.register(dispatcher: CommandDispatcher<CommandSourceStack>) {
-//    dispatcher.register(this)
-//}
