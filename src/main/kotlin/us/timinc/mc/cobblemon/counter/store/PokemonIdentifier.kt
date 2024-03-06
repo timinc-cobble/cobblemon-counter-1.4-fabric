39136a4ab2239ee1704ec39a447fe2903c688010
Callum Birks <callumbirks@protonmail.com>
2024-03-06 21:37:13 +0000

    Remove accommodation for Pikachu "base" form.

--- src/main/kotlin/us/timinc/mc/cobblemon/counter/store/PokemonIdentifier.kt
+++ src/main/kotlin/us/timinc/mc/cobblemon/counter/store/PokemonIdentifier.kt
@@ -1,16 +1,16 @@
 package us.timinc.mc.cobblemon.counter.store
 
 class PokemonIdentifier(val species: String, form: String) {
-    // Filter out "Normal" form to "". Pikachu's (and maybe others?) Normal form is "base".
-    val form: String = if (form.lowercase() == "normal" || form.lowercase() == "base") "" else form
+    // Filter out "Normal" form to "".
+    val form: String = if (form.lowercase() == "normal") "" else form
 
     companion object {
         fun fromJsonKey(key: String): PokemonIdentifier {
             val strings = key.split('+', limit = 2)
             return if (strings.size == 1) PokemonIdentifier(strings[0].replaceFirstChar { it.uppercaseChar() }, "")
             else PokemonIdentifier(
-                strings[0].replaceFirstChar { it.uppercaseChar() },
-                strings[1].replaceFirstChar { it.uppercaseChar() })
+                    strings[0].replaceFirstChar { it.uppercaseChar() },
+                    strings[1].replaceFirstChar { it.uppercaseChar() })
         }
     }
 
